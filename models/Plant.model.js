da9f5f3fbb24c6172bd0cc05b9292f7d915ec6d4
“Konstantinos <kostaks@outlook.com>
2023-03-01 22:53:17 +0100

    merging done and models modified

---
+++ models/Plant.model.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+const plantSchema = new Schema(
+    {
+      Variety: {
+        type: String,
+        required: true,
+      },
+      Size: {
+        type: [String],
+        required: true,
+      },
+      Age: {
+        type: String,
+        required: true,
+      },
+      Price: {
+        type: Number,
+        required: true,
+      },
+      description: {
+        type: String,
+      },
+      owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      image: {
+          type: String,
+          default: ''
+      },  
+    },
+    {
+      timestamps: true
+    }
+  );
+
+const Plant = model("Plant", plantSchema);
+
+module.exports = Plant;

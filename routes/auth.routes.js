6048637ad779b6ce780f87cc6a81efd02d616ae3
“Konstantinos <kostaks@outlook.com>
2023-03-10 13:52:45 +0100

    changes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -81,7 +81,7 @@ router.get("/:username", async (req, res, next) => {
     console.log(req.params.username)
     const username = req.params.username;
     const profile = await User.findOne({username : username})
-    const data = {image: profile.image, id: profile._id , username: profile.username , location : profile.location , number: profile.number}
+    const data = { id: profile._id , username: profile.username , location : profile.location , number: profile.number}
     console.log(data)
     res.status(200).json(data);
 }
